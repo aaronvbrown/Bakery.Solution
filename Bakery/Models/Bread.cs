a8b8047bc767ba47358fe0379fa552d2e37e50b7
Aaron Brown <worldofaaron@hotmail.com>
2023-09-10 08:54:24 -0700

    start actual code

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,9 +1,20 @@
-
+using System;
+using System.Collections.Generic;
 
 namespace Bakery.Models
 {
   public class BreadOrder
   {
+    public int BreadQuantity {get; set;}
+
+    public SetBreadQuantity(int breadQty)
+    {
+      BreadQuantity = breadQty;
+    }
     
+    public decimal GetAmount()
+    {
+      return BreadQuantity * 5
+    }
   }
 }
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,7 +7,21 @@ namespace Bakery
   {
     static void Main()
     {
-      
+      Console.WriteLine("--------------------------");
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("How many loaves of bread would you like to purchase?");
+      string breadInput = Console.ReadLine();  
+      int breadInputQty = int.Parse(breadInput);  
+      BreadOrder breadOrd = new BreadOrder(breadInputQty);
+      ConfirmOrEditBreadQty(breadOrd);
+    }
+
+    static void ConfirmOrEditBreadQty(BreadOrder breadOrd)
+    {
+      Console.WriteLine("Confirm bread order quantity:");
+      Console.WriteLine($"Side 1 has a length of {tri.Side1}.");
+      Console.WriteLIne($"You requested {breadOrd.BreadQuantity}");
+      Console.WriteLIne("Is that right?  Type 'yes' to confirm or 'no' to re-enter the bread order quantity.")
     }
   }
 }
